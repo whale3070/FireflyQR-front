8051cca511986962e96468204f6404fabe18cdd4
whale3070 <3222479461@qq.com>
2026-01-30 05:27:04 +0000

    feat: 部署金库协议1.0工厂合约并对齐Solidity版本

--- monad-nft/script/Deploy.s.sol
+++ monad-nft/script/Deploy.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 import "forge-std/Script.sol";
-import "../src/BookVault.sol";
+import "../src/BookFactory.sol";
 
 contract DeployScript is Script {
     function run() external {
